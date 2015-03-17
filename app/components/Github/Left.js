42926e359a3c252f22cb120c0985b64fc305b88a
David Kapp <davekapp@gmail.com>
2015-03-17 17:03:46 -0500

    fix issue with values being 0 causing bad output

--- app/components/Github/Left.js
+++ app/components/Github/Left.js
@@ -37,16 +37,16 @@ var Left = React.createClass({
       <div>
         <h3>User Profile</h3>
         <ul className="list-group">
-          {this.state.bio.avatar_url && <li className="list-group-item"> <img src={this.state.bio.avatar_url} className="img-rounded img-responsive"/> </li>}
-          {this.state.bio.name && <li className="list-group-item"> Name: {this.state.bio.name} </li>}
-          {this.state.bio.login && <li className="list-group-item"> Username: {this.state.bio.login} </li>}
-          {this.state.bio.email && <li className="list-group-item"> Email: {this.state.bio.email} </li>}
-          {this.state.bio.location && <li className="list-group-item"> Location: {this.state.bio.location} </li>}
-          {this.state.bio.company && <li className="list-group-item"> Company: {this.state.bio.company} </li>}
-          {this.state.bio.followers && <li className="list-group-item"> Followers: {this.state.bio.followers} </li>}
-          {this.state.bio.following && <li className="list-group-item"> Following: {this.state.bio.following} </li>}
-          {this.state.bio.following && <li className="list-group-item"> Public Repos: {this.state.bio.public_repos} </li>}
-          {this.state.bio.blog && <li className="list-group-item"> Blog: <a href={this.state.bio.blog}> {this.state.bio.blog} </a></li>}
+          {this.state.bio.avatar_url && <li className="list-group-item avatar-url"> <img src={this.state.bio.avatar_url} className="img-rounded img-responsive"/> </li>}
+          {this.state.bio.name && <li className="list-group-item name"> Name: {this.state.bio.name} </li>}
+          {this.state.bio.login && <li className="list-group-item login"> Username: {this.state.bio.login} </li>}
+          {this.state.bio.email && <li className="list-group-item email"> Email: {this.state.bio.email} </li>}
+          {this.state.bio.location && <li className="list-group-item location"> Location: {this.state.bio.location} </li>}
+          {this.state.bio.company && <li className="list-group-item company"> Company: {this.state.bio.company} </li>}
+          {this.state.bio.followers !== 0 && <li className="list-group-item followers"> Followers: {this.state.bio.followers} </li>}
+          {this.state.bio.following !== 0 && <li className="list-group-item following"> Following: {this.state.bio.following} </li>}
+          {this.state.bio.public_repos !== 0 && <li className="list-group-item public-repos"> Public Repos: {this.state.bio.public_repos} </li>}
+          {this.state.bio.blog && <li className="list-group-item blog"> Blog: <a href={this.state.bio.blog}> {this.state.bio.blog} </a></li>}
         </ul>
       </div>
     );
